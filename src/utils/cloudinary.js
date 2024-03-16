ff921640b60ce4b3a70abaa5b99bea755bdef458
Muhammad Umer <umertariq7a@gmail.com>
2024-03-16 19:30:31 +0500

    Register User api

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -5,17 +5,18 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
   try {
-    if (localFilePath) return null;
-    //Upload file on cloudinary
+    if (!localFilePath) return null;
+
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    // File has been uploaded succesfully
-    console.log("File has been uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localFilePath);
+
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // Remove the locally saved temp file as the upload fails
